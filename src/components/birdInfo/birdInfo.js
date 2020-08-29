bc4d490be4756a80237a0b43ad270c51921f939e
Yuliya Reutovich <yrevtovich@mail.ru>
2020-08-29 13:11:36 +0300

    feat: add bird info block and next button

--- src/components/birdInfo/birdInfo.js
+++ src/components/birdInfo/birdInfo.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import s from './birdInfo.module.css';
 
-const BirdInfo = () => {
-  return <></>;
+const BirdInfo = ({ data }) => {
+  const { name, species, description, image, audio } = data;
+  return (
+    <div className={s.birdInfo}>
+      <div className={s.birdInfo__main}>
+        <img className={s.birdInfo__image} src={image} />
+        <div className={s.birdInfo__about}>
+          <h3 className={s.birdInfo__name}>{name}</h3>
+          <p className={s.birdInfo__species}>{species}</p>
+          <audio className={s.birdInfo__audio} src={audio}/>
+        </div>
+      </div>
+      <p className={s.birdInfo__description} >{description}</p>
+    </div>
+  );
 }
 
 export default BirdInfo;
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import s from './main.module.css';
 import QuestionBlock from '../questionBlock/questionBlock';
 import GameBlock from '../gameBLock/gameBlock';
+// import Button from '../button/button';
 
 const Main = () => {
   return (
     <main>
       <QuestionBlock />
       <GameBlock />
+      <button className={s.nextBtn}>Next level</button>
     </main>
   )
 }
